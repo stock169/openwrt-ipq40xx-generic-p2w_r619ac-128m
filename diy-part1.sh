c20017df1bd88475188004310f21bac2a201933b
stock169 <58556976+stock169@users.noreply.github.com>
2023-02-13 13:30:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i '$a\src-git OpenClash https://github.com/vernesong/OpenClash'  feeds.conf.default
+#sed -i '$a\src-git OpenClash https://github.com/vernesong/OpenClash'  feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
