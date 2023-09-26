3cfd5055c68d0f8f13ca0a42707d7f10d8d3c127
stock169 <58556976+stock169@users.noreply.github.com>
2023-09-26 23:11:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,9 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i 's/4.3.9_v1.2.14/4.3.8_v1.2.14/g' package/lean/qBittorrent-static/Makefile
 
+
 #wget -P feeds/packages/multimedia/minidlna/patches/ https://raw.githubusercontent.com/stock169/openwrt-ipq40xx-generic-p2w_r619ac-128m/main/002-support-ape-mka-format.patch
+rm -f feeds/packages/multimedia/minidlna/patches/005-added-support-RMVB.patch
 wget -P feeds/packages/multimedia/minidlna/patches/ https://raw.githubusercontent.com/stock169/openwrt-ipq40xx-generic-p2w_r619ac-128m/main/002-support-ape-mka-rmvb-dsd-format.patch
 
 cd feeds/packages/multimedia/
