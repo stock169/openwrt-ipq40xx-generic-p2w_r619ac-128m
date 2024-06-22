892b0d9c6152c4d161debacebcd5c6d6065f96f4
stock169 <58556976+stock169@users.noreply.github.com>
2024-06-23 00:56:36 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i '$a\src-git OpenClash https://github.com/vernesong/OpenClash'  feeds.conf.default
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall2' >> feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+
+#echo "src-git v2raya https://github.com/v2rayA/v2raya-openwrt.git" >> "feeds.conf.default"
+#echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git' >> feeds.conf.default
+#echo 'src-git v2raygeodata https://github.com/sbwml/v2ray-geodata.git' >> feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,54 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/4.3.9_v1.2.14/4.3.8_v1.2.14/g' package/lean/qBittorrent-static/Makefile
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#rm -f feeds/packages/multimedia/minidlna/patches/005-added-support-RMVB.patch
+#wget -P feeds/packages/multimedia/minidlna/patches/ https://raw.githubusercontent.com/stock169/openwrt-ipq40xx-generic-p2w_r619ac-128m/main/002-support-ape-mka-rmvb-dsd-format.patch
+wget -P feeds/packages/multimedia/minidlna/patches/ https://raw.githubusercontent.com/stock169/openwrt-ipq40xx-generic-p2w_r619ac-128m/main/002-support-ape-mka-format.patch
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall/.git
+rm -rf feeds/luci/applications/luci-app-passwall/.github
+mv feeds/luci/applications/luci-app-passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall/luci-app-passwall
+rm -rf feeds/packages/net/ipt2socks
+rm -rf feeds/packages/net/pdnsd-alt
+rm -rf feeds/packages/net/simple-obfs
+rm -rf feeds/packages/net/tcping
+rm -rf feeds/packages/net/trojan-plus
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/xray-plugin
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/microsocks
+rm -rf feeds/packages/net/shadowsocks-rust
+rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/trojan
+rm -rf feeds/packages/net/tuic-client
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/dns2socks
+rm -rf feeds/packages/net/hysteria
+rm -rf feeds/packages/net/naiveproxy
+rm -rf feeds/packages/net/shadowsocksr-libev
+rm -rf feeds/packages/net/ssocks
+rm -rf feeds/packages/net/trojan-go
+rm -rf feeds/packages/net/v2ray-core
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/brook
+rm -rf feeds/packages/net/dns2tcp
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git feeds/packages/net/tmp
+rm -rf feeds/packages/net/tmp/.git
+rm -rf feeds/packages/net/tmp/.github
+rm -rf feeds/packages/net/tmp/gn
+mv feeds/packages/net/tmp/* feeds/packages/net
