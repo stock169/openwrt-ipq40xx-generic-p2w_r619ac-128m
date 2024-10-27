e2827b4d52fd24cf85fbc3374b0815c110bbe35a
stock169 <58556976+stock169@users.noreply.github.com>
2024-10-27 15:14:03 +0800

    Update c-diy-part1.sh

--- c-diy-part1.sh
+++ c-diy-part1.sh
@@ -3,4 +3,4 @@ wget -P packages/devel/gmp/ https://raw.githubusercontent.com/stock169/CoreELEC/
 rm packages/debug/memtester/package.mk
 wget -P packages/debug/memtester/  https://raw.githubusercontent.com/stock169/CoreELEC/refs/heads/coreelec-22/packages/debug/memtester/package.mk
 rm packages/network/bluez/package.mk
-wget -P packages/network/bluez/ https://github.com/stock169/CoreELEC/raw/refs/heads/coreelec-22/packages/network/bluez/package.mk 
+wget -P packages/network/bluez/ https://raw.githubusercontent.com/stock169/CoreELEC/refs/heads/coreelec-22/packages/network/bluez/package.mk
