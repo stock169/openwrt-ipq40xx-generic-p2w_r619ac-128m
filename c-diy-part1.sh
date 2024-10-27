7d5bb18caa8fad42adee9b6a83efd24a99b54411
stock169 <58556976+stock169@users.noreply.github.com>
2024-10-27 14:48:52 +0800

    Create c-diy-part1.sh

---
+++ c-diy-part1.sh
@@ -0,0 +1,6 @@
+rm packages/devel/gmp/package.mk
+wget https://raw.githubusercontent.com/stock169/CoreELEC/refs/heads/coreelec-22/packages/devel/gmp/package.mk packages/devel/gmp/package.mk
+rm packages/debug/memtester/package.mk
+wget https://github.com/stock169/CoreELEC/raw/refs/heads/coreelec-22/packages/debug/memtester/package.mk packages/debug/memtester/package.mk
+rm packages/network/bluez/package.mk
+wget https://github.com/stock169/CoreELEC/raw/refs/heads/coreelec-22/packages/network/bluez/package.mk packages/network/bluez/package.mk
